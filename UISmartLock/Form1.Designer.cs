2630f9dcad510d2e6d04e7c3f8861ff0e921366b
totaldict <balin87@mail.ru>
2018-12-08 18:26:51 +0500

    Ввел WinForms вместо WPF для теста. Добавлено рисование и сохранение рисунка ключа в файл.

---
+++ UISmartLock/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace UISmartLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicBox
+            // 
+            this.PicBox.BackColor = System.Drawing.Color.White;
+            this.PicBox.Location = new System.Drawing.Point(0, -1);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(285, 223);
+            this.PicBox.TabIndex = 0;
+            this.PicBox.TabStop = false;
+            this.PicBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseDown);
+            this.PicBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.PicBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PicBox_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PicBox);
+            this.Name = "Form1";
+            this.Text = "SmartLock";
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UISmartLock/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UISmartLock
+{
+    public partial class Form1 : Form
+    {
+        
+
+        private Point start;
+        private bool drawing = false;
+        private Image orig;
+        Bitmap bm;
+        Bitmap bm2;
+        public Form1()
+        {            
+            InitializeComponent();
+            bm = new Bitmap(PicBox.Width, PicBox.Height);
+            bm2 = new Bitmap(PicBox.Width, PicBox.Height);
+
+        }
+        private void NewPic()
+        {
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!drawing) return;
+            var finish = new Point(e.X, e.Y);
+            bm2 = new Bitmap(bm);
+            PicBox.Image = bm2;
+            var pen = new Pen(Color.Black, 1f);
+            var g = Graphics.FromImage(bm2);
+            g.DrawLine(pen, start, finish);
+            g.Dispose();
+            PicBox.Invalidate();
+            
+
+        }
+
+        private void PicBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = new Point(e.X, e.Y);
+            orig = bm;
+            drawing = true;
+        }
+
+        private void PicBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            var finish = new Point(e.X, e.Y);
+            var g = Graphics.FromImage(bm);
+            var pen = new Pen(Color.Black, 1f);
+            g.DrawLine(pen, start, finish);
+            g.Save();
+            drawing = false;
+            g.Dispose();
+            PicBox.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //вот тут нормальные имена ключей сделать
+            string path = @"D:\TestKey\test"+DateTime.Now.DayOfYear+DateTime.Now.Second+".jpg";
+            PicBox.Image.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+            //разобраться с тем что сохраняется чёрная херня вместо изображения
+        }
+    }
+}
